4271626cf15ff44b8ec216f92d1ac56ad42e4ed0
fansted <2858661142@qq.com>
2016-12-01 18:47:33 +0800

    2nd_iteration_Invite

---
+++ src/Data/InviteTeacher.java
@@ -0,0 +1,25 @@
+package Data;
+
+public class InviteTeacher {
+	private int Id;
+	private String Name;
+	private String Academy;
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getAcademy() {
+		return Academy;
+	}
+	public void setAcademy(String academy) {
+		Academy = academy;
+	}
+}
